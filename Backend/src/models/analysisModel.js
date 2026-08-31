0dd1359183cee70ac2b4f76479d736a386c74dc0
Anshika Sengar <sengaranshika94@gmail.com>
2026-08-31 15:34:16 +0530

    added analysisModel

---
+++ Backend/src/models/analysisModel.js
@@ -0,0 +1,54 @@
+const mongoose = requie("mongoose")
+const analysisSchema = new mongoose.Schema({
+    design: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "designs",
+        required: true
+    },
+
+    overallScore: {
+        type: Number,
+        required: true
+    },
+
+    categoryScores: {
+        visualDesign: {
+            type: Number,
+            required: true
+        },
+
+        ux: {
+            type: Number,
+            required: true
+        },
+
+        accessibility: {
+            type: Number,
+            required: true
+        },
+
+        typography: {
+            type: Number,
+            required: true
+        },
+
+        layout: {
+            type: Number,
+            required: true
+        },
+
+        consistency: {
+            type: Number,
+            required: true
+        }
+    },
+
+    issues: {
+        type: Array,
+        default: []
+    }
+}, { timestamps: true })
+
+const analysisModel = mongoose.model("analyses", analysisSchema)
+
+module.exports = analysisModel
