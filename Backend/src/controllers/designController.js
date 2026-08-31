3dd698262769a6765eec158943952e611a305478
Anshika Sengar <sengaranshika94@gmail.com>
2026-08-31 15:15:04 +0530

    design controller is completed

--- Backend/src/controllers/designController.js
+++ Backend/src/controllers/designController.js
@@ -1,5 +1,6 @@
 const designModel = require('../models/designModel')
 const imagekit = require('../config/imagekit')
+
 async function createDesign(req, res) {
     try {
         console.log("1. Controller reached")
@@ -34,4 +35,61 @@ async function createDesign(req, res) {
     }
 }
 
-module.exports = {createDesign}
+async function getDesigns(req,res){
+    try{
+        const designs = await designModel.find({
+        user:req.user.id
+    })
+    return res.status(200).json({
+        message:"Designs fetched successfully",
+        designs
+    })
+    }catch(err){
+        console.log("GET DESIGNS ERROR",err);
+        return res.status(500).json({
+            message:"Failed to fetch designs"
+        })
+    }
+}
+
+async function getDesign(req,res){
+
+    const id= req.params.id
+    const design = await designModel.findOne({
+        _id:id,             //checks requested design id
+        user:req.user.id    //checks logged-in user id
+    })
+
+     if (!design) {
+        return res.status(404).json({
+            message: "Design not found"
+        })
+    }
+
+    return res.status(200).json({
+        message:"Design fetched successfully",
+        design
+    })
+}
+
+async function deleteDesign(req,res){
+    const id = req.params.id
+    const result= await designModel.deleteOne({ 
+        _id:id,
+        user:req.user.id
+    })
+    if (result.deletedCount === 0) {
+            return res.status(404).json({
+            message: "Design not found"
+        })
+    }
+    return res.status(200).json({
+        message:"Design deleted successfully"
+    })
+}
+
+module.exports = {createDesign,
+    getDesigns,
+    getDesign,
+    deleteDesign
+}
--- Backend/src/routes/design.routes.js
+++ Backend/src/routes/design.routes.js
@@ -5,5 +5,7 @@ const designController = require("../controllers/designController")
 const upload = require('../middlewares/uploadMiddleware')
 //upload.single("screenshot"):I'm expecting one file, and the field name of that file must be screenshot.
 designRouter.post('/',authMiddleware,upload.single("screenshot"),designController.createDesign)
-
+designRouter.get('/',authMiddleware,designController.getDesigns)
+designRouter.get('/:id',authMiddleware,designController.getDesign)
+designRouter.delete('/:id',authMiddleware,designController.deleteDesign)
 module.exports = designRouter
